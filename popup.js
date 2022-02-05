13b80769b3db7069d6726a2ab2826fb471f85e7c
lhsueh1 <lhsueh1@binghamton.edu>
2022-02-05 14:58:07 -0500

    new cat

--- popup.js
+++ popup.js
@@ -2,23 +2,30 @@
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+    changeColor.style.backgroundColor = color;
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: paste_crow,
+    });
 });
 
 // The body of this function will be executed as a content script inside the
 // current page
 function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+    chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = color;
+    });
+}
+
+function paste_crow() {
+    // const image = document.createElement('img')
+    // image.src  = "image0.gif"
+    // document.getElementById('body').appendChild(image);
+    document.write("<img src='image0.gif' alt='alt tag'>")
 }
