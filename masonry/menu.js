fe91b871a97ea2598dc89cabafcde05abdbf5a5f
Ya Ting Sun (Candy) <ysun106@binghamton.edu>
2022-02-06 00:20:05 -0500

    menu format ok but can not toggle visibility

--- masonry/menu.js
+++ masonry/menu.js
@@ -1,27 +1,28 @@
 // this section of the program should allow you to open the main menu when clicking on the cat
 let click = true;
+let messageBox = $(".message");
 const cat = document.querySelector(".i1");
 if (cat) {
     cat.addEventListener('click', openMenu);
 }
 
-function showImage(image) {
+/*function showImage(image) {
     document.getElementById(image).style.visibility = "visible";
 }
 
 function hideImage(image) {
     document.getElementById(image).style.visibility = "hidden";
-}
+}*/
 
 function openMenu(event) {
     // first click should open the menu, second click on cat will close the menu
     console.log("hello world");
     if (click == true) {
-        showImage("fact");
+        messageBox.css({visability = visible});
         click = false;
     }
     else {
-        hideImage("fact");
+        messageBox.css({visability = hidden});
         click = true;
     }
 }
